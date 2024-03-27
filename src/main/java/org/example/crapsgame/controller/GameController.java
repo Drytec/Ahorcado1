419462fbbbbaf4d212d045ee136fd55a706bf2d9
NickCoder <NickCoder1309@users.noreply.github.com>
2024-03-26 21:01:33 -0500

    listo

--- src/main/java/org/example/crapsgame/controller/GameController.java
+++ src/main/java/org/example/crapsgame/controller/GameController.java
@@ -13,6 +13,9 @@ import javafx.scene.layout.StackPane;
 import org.example.crapsgame.model.player.Player;
 import org.example.crapsgame.model.alert.AlertBox;
 
+import java.util.ArrayList;
+import java.util.List;
+
 
 public class GameController {
     @FXML
@@ -21,25 +24,47 @@ public class GameController {
     private ImageView ahorcado;
     @FXML
     private TextField letraParaComprobar;
+    @FXML
+    private TextField palabraParaComprobar;
+    @FXML
+    private HBox HBoxLetters;
     private static String wordToFind;
     private char[] wordLetters;
-    private boolean letraCoincide;
+    private int imageNumber = 0;
+    List<Integer> positionList = new ArrayList<>();
 
     @FXML
     void initialize() {
-        ahorcado = new ImageView();
-
-        anchorPane.getChildren().addAll(ahorcado);
+        descomponerPalabra(wordToFind);//Se descompone la palabra en un Array de letras Char y se muestra cantidad de letras
     }
 
     @FXML
-    void OnHandleButtonThrowDice(ActionEvent event) {
-
-        ahorcado.setImage(new Image(String.valueOf(getClass().getResource("/org/example/crapsgame/Images/6.png"))));
-        //centra la imagen
-
-        descomponerPalabra(wordToFind);//Hay que hacer el get y ponerlo en la funcion luego hacer if y ya gg
-        letraCoincide = comprobarTexto(wordLetters, letraParaComprobar.getText().charAt(0));
+    void OnHandleButtonVerifyWord(ActionEvent event) {
+        boolean sameWord = comprobarPalabra(palabraParaComprobar.getText(), wordToFind);
+        if (sameWord){
+            for(int i = 0; i < wordLetters.length; i++){
+                Label wordLabel = (Label) HBoxLetters.getChildren().get(i);
+                wordLabel.setText(String.valueOf(wordLetters[i]));
+            }
+        }
+        else {
+            imageNumber++;
+            ahorcado.setImage(new Image(String.valueOf(getClass().getResource("/org/example/crapsgame/Images/"+imageNumber+".png"))));
+        }
+    }
+    @FXML
+    void OnHandleButtonVerifyLetter(ActionEvent event){
+        positionList = comprobarLetra(wordLetters, letraParaComprobar.getText().charAt(0));
+        if (positionList.isEmpty()){
+            imageNumber++;
+            ahorcado.setImage(new Image(String.valueOf(getClass().getResource("/org/example/crapsgame/Images/"+imageNumber+".png"))));
+        }
+        else {
+            for (int i = 0; i < positionList.size(); i++ ) {
+                Label lettersLabel = (Label) HBoxLetters.getChildren().get(positionList.get(i));
+                lettersLabel.setText(String.valueOf(wordLetters[positionList.get(i)]));
+            }
+        }
     }
 
     @FXML
@@ -63,27 +88,35 @@ public class GameController {
     private Player player;
 
     public void descomponerPalabra(String word) {
-        HBox hbox= new HBox();
-        hbox.setSpacing(10);
+        HBoxLetters.setSpacing(10);
         wordLetters = word.toCharArray();
         for (char c : wordLetters) {
-            Label label = new Label(String.valueOf(c));
+            Label label = new Label(String.valueOf("__"));
             label.setStyle("-fx-font-size: 20px; -fx-font-weight: bold; -fx-font-family: 'Sans Serif';");
-            hbox.getChildren().add(label);
-
+            HBoxLetters.getChildren().add(label);
         }
-        anchorPane1.getChildren().add(hbox);
     }
 
-    public boolean comprobarTexto(char[] charArray, char letter) {
-        boolean matchingLetter = false;
-        for(char c : charArray) {
-            if(c == letter ){
-                matchingLetter = true;
-                break;
+    public boolean comprobarPalabra(String word1, String word2){
+        boolean sameWord = true;
+        char[] word1Letters = word1.toCharArray();
+        char[] word2Letters = word2.toCharArray();
+        for(int i = 0; i < word1Letters.length; i++){
+            if(word1Letters[i] != word2Letters[i]){
+                sameWord = false;
             }
         }
-        return matchingLetter;
+        return sameWord;
     }
 
+
+    public List<Integer> comprobarLetra(char[] charArray, char letter) {
+        List<Integer> positionArray = new ArrayList<>();
+        for(int i = 0; i < charArray.length; i++) {
+            if(charArray[i] == letter ){
+                positionArray.add(i);
+            }
+        }
+        return positionArray;
+    }
 }
